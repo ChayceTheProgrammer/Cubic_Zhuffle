1d1cdac5b44576819ad7a01ed8e909bed0c079b9
ChayceTheProgrammer <104463214+ChayceTheProgrammer@users.noreply.github.com>
2022-05-09 22:58:33 -0500

    Add files via upload
    Day 1 Made Good Progress. Got Camera Movement, Player Input, collision, and a soft game over (disabling player control after a collision)

---
+++ Follow_Player.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow_Player : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    /*
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+        // UnityEngine.Debug.Log(player.position);
+        
+        transform.position = player.position + offset;
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+This directory is where I will keep all of the scripts for Cubic Zhuffle: ReZuffled.
